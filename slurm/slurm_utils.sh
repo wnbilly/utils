8a467ac3e650aedc3ae1ec33a5f1935a89798d21
Billy WEYNANS <weynansbilly@gmail.com>
2023-07-25 15:13:18 +0200

    added sljob, impr sout and stail

--- slurm/slurm_utils.sh
+++ slurm/slurm_utils.sh
@@ -11,6 +11,10 @@
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+# Parameters
+SQUEUE_REFRESH_RATE=3 # Refresh rate of squeue in seconds
+NB_LINES_STAIL=25 # Number of lines to show in stail
+
 # Better sbatch to store the latest job id and a better display of the output
 function run_sbatch() {
 	output=$(sbatch "$1")
@@ -21,14 +25,47 @@ function run_sbatch() {
 
 # alias sbatch="run_sbatch" # May cause issues with sbatch command
 
+# Easier access to the command
+alias squ="squeue -u $USER -i $SQUEUE_REFRESH_RATE"
+
+# sljob to manually set LATEST_JOB
+function sljob() {
+  export LATEST_JOB="$1"
+}
+
+# stail to tail the latest job or the logs of a specific job if provided
+# It is supposed to be launched from slurm_scripts
+function stail() {
+  if [[ -z $1 ]]; then
+    # If LATEST_JOB contains a job id
+    if [[ -n $LATEST_JOB ]]; then
+      latest_file=logs/job$LATEST_JOB.out
+    # Else get the last modified file
+    else
+      latest_file=$(ls -t logs/job*.out | head -n 1)
+    fi
+    echo -e "${YELLOW}---$latest_file---${NC}"
+    tail -f -n $NB_LINES_STAIL "$latest_file"
+  else
+    echo -e "${YELLOW}---logs/job$1.out---${NC}"
+    tail -f -n $NB_LINES_STAIL "logs/job$1.out" # Requires format jobXXX.out
+  fi
+}
+
 # sout to show .out of your last job
 # It is supposed to be launched from slurm_scripts. I kept sout as tail only shows the last 10 lines by default
 function sout() {
   if [[ -z $1 ]]; then
-    latest_file=$(ls -t logs/job*.out | head -n 1)
+    # If LATEST_JOB contains a job id
+    if [[ -n $LATEST_JOB ]]; then
+      latest_file=logs/job$LATEST_JOB.out
+    # Else get the last modified file
+    else
+      latest_file=$(ls -t logs/job*.out | head -n 1)
+    fi
     echo -e "${YELLOW}---$latest_file---${NC}"
     cat "$latest_file"
-    echo -e "${YELLOW}---logs/job$1.out---${NC}"
+    echo -e "${YELLOW}---$latest_file---${NC}"
   else
     echo -e "${YELLOW}---logs/job$1.out---${NC}"
     cat "logs/job$1.out" # Requires format jobXXX.out
@@ -51,21 +88,3 @@ function ssj() {
     echo "Please provide a job id as LATEST_JOB is null."
   fi
 }
-
-# Easier access to the command
-alias squ="squeue -u $USER -i 3"
-
-# stail to tail the latest job or the logs of a specific job if provided
-# It is supposed to be launched from slurm_scripts
-function stail() {
-  if [[ -z $1 ]]; then
-    latest_file=$(ls -t logs/job*.out | head -n 1)
-    echo -e "${YELLOW}---$latest_file---${NC}"
-    tail -f -n 25 "$latest_file"
-  else
-    echo -e "${YELLOW}---logs/job$1.out---${NC}"
-    tail -f -n 25 "logs/job$1.out" # Requires format jobXXX.out
-  fi
-}
-
-# Disclaimer : I chose to use the latest file for stail and showout but LATEST_JOB could be used as well like in ssj
