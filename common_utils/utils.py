6dd621cd039173ca8e3ed126d5ff3594b11a60d0
Billy <102171805+wnbilly@users.noreply.github.com>
2024-07-01 09:12:26 +0200

    Add files via upload
    more utils

--- common_utils/utils.py
+++ common_utils/utils.py
@@ -8,21 +8,66 @@ def verif_path(p):
         os.makedirs(p)
 
 
-def list_folders(path):
-    return [f_ for f_ in os.listdir(path) if os.path.isdir(os.path.join(path, f_))]
+def list_folders(path, reverse_sort=False):
+    return sorted([f_ for f_ in os.listdir(path) if os.path.isdir(os.path.join(path, f_))], reverse=reverse_sort)
 
 
-def list_files(path, file_extension=None):
+def list_files(path, reverse_sort=False, file_extension=None, keep_path=False):
     if file_extension is None:
-        return [f_ for f_ in os.listdir(path) if os.path.isfile(os.path.join(path, f_))]
+        files = [f_ for f_ in os.listdir(path) if os.path.isfile(os.path.join(path, f_))]
     else:
-        return [f_ for f_ in os.listdir(path) if os.path.splitext(f_)[1] == file_extension]
+        files = [f_ for f_ in os.listdir(path) if os.path.splitext(f_)[1] == file_extension]
+
+    files.sort(reverse=reverse_sort)
+
+    return [os.path.join(path, f_) for f_ in files] if keep_path else files
 
 
-def imshow_and_wait(img, window_name="Image"):
+def list_files_recursively(start_path):
+    files = []
+    for root, dirs, files in os.walk(start_path):
+        files += [os.path.join(root, file) for file in files]
+    return files
+
+
+def imshow_and_wait(img, window_name="imshow", window_x=0, window_y=0):
     cv.imshow(window_name, img)
+    cv.moveWindow(window_name, window_x, window_y)
     cv.waitKey(0)
-    cv.destroyWindow(window_name)
+    if cv.getWindowProperty(window_name, cv.WND_PROP_VISIBLE):
+        cv.destroyWindow(window_name)
+
+
+def image_resize(image, width=None, height=None, inter=cv.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
 
 
 def load_json(load_path):
@@ -32,3 +77,18 @@ def load_json(load_path):
 def save_json(data, save_path):
     with open(save_path, "w") as save_file:
         json.dump(data, save_file, ensure_ascii=True, indent=4)
+
+
+def append_json(data, save_path):
+    previous_data = load_json(save_path)
+    previous_data.update(data)
+    save_json(previous_data, save_path)
+
+
+def argsort(seq):
+    # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
+    return sorted(range(len(seq)), key=lambda i: seq[i])
+
+
+def sort_by(seq, sort_key):
+    return [seq[idx] for idx in argsort(sort_key)]
